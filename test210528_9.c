ccf3c3b6d8d46838094f6587f881d588136f19fe
lbt <2209823792@qq.com>
2021-05-28 23:52:58 +0800

    斐波那契

---
+++ test210528_9.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+
+#include<stdio.h>
+#include<string.h>
+// µÝ¹éÇóì³²¨ÄÇÆõ
+int fib1(int num) {
+	if (num == 1 || num == 2) {
+		return 1;
+	}
+	return fib1(num - 1) + fib1(num - 2);
+}
+// ·ÇµÝ¹éÇóì³²¨ÄÇÆõ
+int fib2(int num) {
+	if (num == 1 || num == 2) {
+		return 1;
+	}
+	int pre = 1;
+	int ppre = 1;
+	int now = pre + ppre;
+	for (int i = 3; i <= num; i++) {
+		now = pre + ppre;
+		ppre = pre;
+		pre = now;
+	}
+	return now;
+}
+int main() {
+	printf("%d\n", fib1(5));
+	printf("%d\n", fib2(5));
+	return 0;
+}
