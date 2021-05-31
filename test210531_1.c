10ac648722291cd58ee11b5c338abe703c3ef0a5
lbt <2209823792@qq.com>
2021-05-31 21:46:24 +0800

    实践出真知

---
+++ test210531_1.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+int main() {
+	printf("Practice makes perfect!");
+	return 0;
+}
