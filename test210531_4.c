a15ab1deca22cab56ae42efbfa334265ddf27ea3
lbt <2209823792@qq.com>
2021-05-31 21:58:21 +0800

    小飞机

---
+++ test210531_4.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main() {
+	printf("     **\n");
+	printf("     **\n");
+	printf("************\n");
+	printf("************\n");
+	printf("    *  *\n");
+	printf("    *  *\n");
+	return 0;
+}
