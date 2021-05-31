12ae5806cbe5f3e9e583536e475057618cf3fb94
lbt <2209823792@qq.com>
2021-05-31 21:47:56 +0800

    我是大V

---
+++ test210531_2.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main() {
+	printf("v   v\n");
+	printf(" v v\n");
+	printf("  v\n");
+	return 0;
+}
