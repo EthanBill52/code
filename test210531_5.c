65f1a3d0742fcf38b1053cbbdf9d78305caa6280
lbt <2209823792@qq.com>
2021-05-31 22:05:30 +0800

    缩短二进制

---
+++ test210531_5.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main() {
+	printf("0%o 0X%X\n", 1234, 1234);
+	return 0;
+}
